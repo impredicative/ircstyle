73790cce03e3ddfa0dda0224285faa95f2e1a16f
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-10 23:50:32 -0500

    Color constants

---
+++ ircmessage/colors.py
@@ -0,0 +1,23 @@
+white   = 0
+black   = 1
+blue    = 2
+green   = 3
+red     = 4
+brown   = 5
+purple  = 6
+orange  = 7
+yellow  = 8
+lime    = 9
+teal    = 10
+aqua    = 11
+royal   = 12
+pink    = 13
+grey    = 14
+silver  = 15
+
+# mIRC Color aliases
+light_green = lime
+cyan        = teal
+light_cyan  = aqua
+light_blue  = royal
+light_grey  = silver
