3dd6d00842686b4dea360df8911130e34655c263
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-11 03:03:09 -0500

    Mapping

--- ircmessage/colors.py
+++ ircmessage/colors.py
@@ -21,3 +21,23 @@ cyan        = teal
 light_cyan  = aqua
 light_blue  = royal
 light_grey  = silver
+
+# Mapping
+idToName = {
+    0: 'white',
+    1: 'black',
+    2: 'blue',
+    3: 'green',
+    4: 'red',
+    5: 'brown',
+    6: 'purple',
+    7: 'orange',
+    8: 'yellow',
+    9: 'lime',
+    10: 'teal',
+    11: 'aqua',
+    12: 'royal',
+    13: 'pink',
+    14: 'grey',
+    15: 'silver'
+}
