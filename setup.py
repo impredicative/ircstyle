f09e298ed140f185af9c9316d6f639ada9659a8f
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-10 23:50:06 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IRC Message Parser',
+    version='0.1.0',
+    description='IRC Message Parser',
+    long_description='IRC Message Parser',
+    author='Makoto Fujimoto',
+    author_email='makoto@makoto.io',
+    url='https://github.com/FujiMakoto/IRC-Message-Parser',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=find_packages(),
+    entry_points={},
+    install_requires=['click', 'six']
+)
